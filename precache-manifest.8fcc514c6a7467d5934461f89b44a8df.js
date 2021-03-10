9877bdb333922d134537d0ab4021ce960c8bfdfe
Aseel <aseelalbarq7@gmail.com>
2021-03-11 01:14:38 +0200

    deploy

---
+++ precache-manifest.8fcc514c6a7467d5934461f89b44a8df.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6130c1de04cb3b57cbce",
+    "url": "/maf-mytask/css/app.8e7b198b.css"
+  },
+  {
+    "revision": "6c63a4b5071bff132272b2168ee373cf",
+    "url": "/maf-mytask/fonts/Larsseit.6c63a4b5.woff2"
+  },
+  {
+    "revision": "72945abd6e419fc09b4ab40dacdea78e",
+    "url": "/maf-mytask/fonts/Larsseit.72945abd.woff"
+  },
+  {
+    "revision": "8f95f19e00e03b7afd899f2c7099b3d9",
+    "url": "/maf-mytask/fonts/Larsseit.8f95f19e.eot"
+  },
+  {
+    "revision": "d5bd523d9152e80458f764c62c5bb9df",
+    "url": "/maf-mytask/fonts/Larsseit.d5bd523d.ttf"
+  },
+  {
+    "revision": "2414093e10109bd9ac59738167c05eb5",
+    "url": "/maf-mytask/fonts/NoeDisplay-Bold.2414093e.woff2"
+  },
+  {
+    "revision": "30a71dcf9a1c39558ca4e97c92a4b2fa",
+    "url": "/maf-mytask/fonts/NoeDisplay-Bold.30a71dcf.ttf"
+  },
+  {
+    "revision": "5221ebefc013e462c0aabcab361b9e42",
+    "url": "/maf-mytask/fonts/NoeDisplay-Bold.5221ebef.eot"
+  },
+  {
+    "revision": "f0c4b9a479fab76f3fd5521e9ad08850",
+    "url": "/maf-mytask/fonts/NoeDisplay-Bold.f0c4b9a4.woff"
+  },
+  {
+    "revision": "84c7387055c2af8ce650c613f0a7fde7",
+    "url": "/maf-mytask/img/NT.84c73870.svg"
+  },
+  {
+    "revision": "2aae9fc727e870ce8e5dc733f77e4d20",
+    "url": "/maf-mytask/img/desert.2aae9fc7.svg"
+  },
+  {
+    "revision": "422f65f2e46fcca7822497d7275e70fe",
+    "url": "/maf-mytask/img/dots.422f65f2.svg"
+  },
+  {
+    "revision": "c3eace36b9e9e69d04924d1f3bcd3081",
+    "url": "/maf-mytask/img/facebook.c3eace36.svg"
+  },
+  {
+    "revision": "9c90448049bef9f70d12e6827dd94077",
+    "url": "/maf-mytask/img/instagram.9c904480.svg"
+  },
+  {
+    "revision": "5cfaa91225e2474f451671fa0a28c68d",
+    "url": "/maf-mytask/img/main.5cfaa912.png"
+  },
+  {
+    "revision": "8297cd8d2ffe4f0a6ce859fadf738e41",
+    "url": "/maf-mytask/img/search.8297cd8d.svg"
+  },
+  {
+    "revision": "d308220be49b2bce75448a396e7169ed",
+    "url": "/maf-mytask/img/twitter.d308220b.svg"
+  },
+  {
+    "revision": "fc88fb05f7772cd57c681e075a4de0d2",
+    "url": "/maf-mytask/index.html"
+  },
+  {
+    "revision": "6130c1de04cb3b57cbce",
+    "url": "/maf-mytask/js/app.21b02f82.js"
+  },
+  {
+    "revision": "0d30d5fa5f76690633c8",
+    "url": "/maf-mytask/js/chunk-vendors.919c39de.js"
+  },
+  {
+    "revision": "87a12734c7623dab55e7bc8186480c79",
+    "url": "/maf-mytask/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/maf-mytask/robots.txt"
+  }
+]);
